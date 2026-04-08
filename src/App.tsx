cdc607f68ae1c2b98f5f089656ad41e62f39d41a
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2026-04-08 10:16:56 +0800

    added update

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ const App: React.FC = () => (
      
         <Route exact path ="/" component={Login} />
         <Route path ="/app" component={Menu} />
+        
        
 
       </IonRouterOutlet>
