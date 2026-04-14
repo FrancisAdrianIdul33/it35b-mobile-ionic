cf74e4b0083ee0cdbcea8c2f738d2daeca1f7137
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2026-04-14 08:13:54 +0800

    added tabs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,36 @@
-import { IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonButtons, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonButtons, IonToolbar, IonTabs, IonTabButton, IonLabel, IonIcon } from '@ionic/react';
 import './Home.css';
 import ExploreContainer from '../components/ExploreContainer';
 import { bookOutline, search, star } from 'ionicons/icons';
+import { IonReactRouter } from '@ionic/react-router';
 
 
 const Home: React.FC = () => {
   const tabs = [
 
-    {name: "Feed", tab:'feed',url:"/app/home/feed",icon:bookOutline},
-     {name: "Favorites", tab:'favorites',url:"/app/home/feed",icon:star},
-      {name: "Search", tab:'search',url:"/app/home/feed",icon:search}
-]
-
-
-  return 
+    { name: "Feed", tab: 'feed', url: "/app/home/feed", icon: bookOutline },
+    { name: "Favorites", tab: 'favorites', url: "/app/home/feed", icon: star },
+    { name: "Search", tab: 'search', url: "/app/home/feed", icon: search }
+  ]
+  return (
     <IonReactRouter>
       <IonTabs>
-        <IonToolBar>
+        <IonToolbar slot="bottom">
           <IonTitle>
             Tabs
-             </IonTitle>
-             </IonToolBar>
-               </IonToolBar>
-                 </IonTabs>
-                 
+          </IonTitle>
+        </IonToolbar>
+        {/**/}
+        {tabs.map((tab, index) => (
+          <IonTabButton key={index} tab={tab.tab} href={tab.url}>
+            <IonIcon icon={tab.icon} />
+            <IonLabel>{tab.name}</IonLabel>
+          </IonTabButton>
+        ))}
+
+      </IonTabs>
     </IonReactRouter>
 
-            
-
-  
   );
 };
 
