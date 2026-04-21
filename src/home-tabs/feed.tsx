60c9cd2741b9ab31159020a02f2e90c55a20bf8a
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2026-04-21 09:02:39 +0800

    added structure 2

--- src/home-tabs/feed.tsx
+++ src/home-tabs/feed.tsx
@@ -1,16 +1,22 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonList } from '@ionic/react';
-import { } from 'react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonGrid, IonCol, IonRow, IonIcon, IonButton } from '@ionic/react';
+import { gitCommitOutline, shareOutline, thumbsUpOutline } from 'ionicons/icons';
+import { useInsertionEffect, useState } from 'react';
 
 const Feed: React.FC = () => {
     const games = [
         { name: 'Pokémon Yellow' },
-         { name: 'Mega Man X' },
-          { name: 'The Legend of Zelda' },
-           { name: 'Pac-man' },
-            { name: 'Super Mario World' },
-        ]
- return (
-       <IonPage>
+        { name: 'Mega Man X' },
+        { name: 'The Legend of Zelda' },
+        { name: 'Pac-man' },
+        { name: 'Super Mario World' },
+    ]
+
+    const [searchText,setSearchText] = useState('');
+    const handleSearch = (event: CustomEvent) => {}
+
+          
+    return (
+        <IonPage>
             <IonHeader>
                 <IonToolbar>
                     <IonButtons slot="start">
@@ -22,15 +28,53 @@ const Feed: React.FC = () => {
 
             <IonContent className='ion-padding'>
                 <IonList>
-                    {games.map((game, index) => (
-                        <IonItem key={index}>
-                            <IonLabel>{game.name}</IonLabel>
-                        </IonItem>
+                    {games.map((item,index) => (
+                        <IonCardHeader>
+                         <img alt="Card Image" src="https://www.nintendo.com/eu/media/images/10_share_images/games_15/game_boy_4/H2x1_GB_PokemonYellow_enGB_image1600w.jpg" />
+                        <IonCardHeader>
+                            <IonCardTitle>{item.name}</IonCardTitle>
+                            <IonCardSubtitle>card subtitle</IonCardSubtitle>
+                        </IonCardHeader>
                     ))}
+
+                    <IonCardContent>
+                 
+                        <IonCardContent>
+                            Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                        </IonCardContent>
+                        <IonGrid>
+                            <IonRow>
+                                <IonCol>
+                                    <IonButton fill="clear" expand="full">
+                                        <IonIcon icon={thumbsUpOutline}></IonIcon>
+                                        <IonLabel style={{marginLeft: '5px'}}>Like</IonLabel>
+                                        </IonButton>
+                                </IonCol>
+                                  <IonCol>
+                                    <IonButton fill="clear" expand="full">
+                                        <IonIcon icon={gitCommitOutline}></IonIcon>
+                                        <IonLabel style={{marginLeft: '5px'}}>Comment</IonLabel>
+                                        </IonButton>
+                                </IonCol>
+                                  <IonCol>
+                                    <IonButton fill="clear" expand="full">
+                                        <IonIcon icon={shareOutline}></IonIcon>
+                                        <IonLabel style={{marginLeft: '5px'}}>Share</IonLabel>
+                                        </IonButton>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonCardContent>
                 </IonList>
             </IonContent>
         </IonPage>
-  );
+
+
+    );
 };
 
 export default Feed;
+
+function setFilterGames(filtered: { name: string; }[]) {
+    throw new Error('Function not implemented.');
+}
--- src/home-tabs/search.tsx
+++ src/home-tabs/search.tsx
@@ -1,7 +1,29 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { } from 'react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonSearchbar, IonTitle, IonToolbar } from '@ionic/react';
+import { SetStateAction, useEffect, useInsertionEffect, useState } from 'react';
 
 const Search: React.FC = () => {
+        const games = [
+            { name: 'Pokémon Yellow' },
+            { name: 'Mega Man X' },
+            { name: 'The Legend of Zelda' },
+            { name: 'Pac-man' },
+            { name: 'Super Mario World' },
+        ]
+    
+        const [searchText,setSearchText] = useState('');
+        const [filtered,setFilteredGames] = useState(games);
+
+        useEffect  (() => {
+        const debounce = setTimeout(() => { 
+            const filtered = games.filter(game=>
+                game.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredGames(filtered);
+        }, 400);
+
+        return () => clearTimeout(debounce);
+    }, [searchText]);
+
     return (
 
         <IonPage>
@@ -14,8 +36,14 @@ const Search: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
 
-            <IonContent fullscreen>
-                <h1>Search</h1>
+ 
+               <IonContent className='ion-padding'>
+                <IonSearchbar
+                    placeholder='Search games'
+                    value={searchText}
+                    debounce={0}
+                    onIonInput={(event: CustomEvent) => setSearchText(event.detail.value!)}
+                />
             </IonContent>
 
         </IonPage>
