1d80b8158db28ce953896918f195775076451e49
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2026-04-21 08:14:52 +0800

    added structure

--- src/home-tabs/feed.tsx
+++ src/home-tabs/feed.tsx
@@ -1,26 +1,36 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonList } from '@ionic/react';
 import { } from 'react';
 
 const Feed: React.FC = () => {
-    return (
-
-        <IonPage>
+    const games = [
+        { name: 'Pokémon Yellow' },
+         { name: 'Mega Man X' },
+          { name: 'The Legend of Zelda' },
+           { name: 'Pac-man' },
+            { name: 'Super Mario World' },
+        ]
+ return (
+       <IonPage>
             <IonHeader>
                 <IonToolbar>
                     <IonButtons slot="start">
                         <IonMenuButton></IonMenuButton>
                     </IonButtons>
-
+                    <IonTitle>Feed</IonTitle>
                 </IonToolbar>
             </IonHeader>
 
-            <IonContent fullscreen>
-                <h1>Feed</h1>
+            <IonContent className='ion-padding'>
+                <IonList>
+                    {games.map((game, index) => (
+                        <IonItem key={index}>
+                            <IonLabel>{game.name}</IonLabel>
+                        </IonItem>
+                    ))}
+                </IonList>
             </IonContent>
-
         </IonPage>
-
-    );
+  );
 };
 
 export default Feed;
